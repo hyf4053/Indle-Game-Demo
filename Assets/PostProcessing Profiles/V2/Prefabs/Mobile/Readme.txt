5416d52009eb9e829aecae95bd5ef8ca6ab387cc
Yufei Han <hyf4053@hotmail.com>
2018-05-25 23:27:05 +0800

    Create Main Menu and setup some basic framework

---
+++ Assets/PostProcessing Profiles/V2/Prefabs/Mobile/Readme.txt
@@ -0,0 +1,2 @@
+v2 Beta not stable,
+Please use v1 post processing profile prefabs for Mobile.
---
+++ Assets/Scripts/UI/OpenURL.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class OpenURL : MonoBehaviour {
+	public string websteAddress;
+
+	public void OpenURLOnClick(){
+		Application.OpenURL(websteAddress);
+	}
+
+}
